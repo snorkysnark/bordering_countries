3804d7bc5da3ce0cb13ae2824d924a7bd7f7c25a
snorkysnark <snorkysnark@gmail.com>
2023-06-29 19:11:29 +0200

    final joined table

--- example.ju.py
+++ example.ju.py
@@ -4,18 +4,8 @@ from SPARQLWrapper import SPARQLWrapper, JSON as JsonReturn
 from tqdm import tqdm
 
 # %%
+# Display longer strings
 pl.Config.set_fmt_str_lengths(100)
-
-# %%
-
-input_countries = (
-    pl.scan_csv("./data/landlocked 2023-06-26 - Лист1.csv")
-    .select(pl.col("landlocked country").alias("name"))
-    .collect()
-    .to_series()
-    .to_list()
-)
-input_countries
 
 
 # %%
@@ -35,30 +25,47 @@ class WikidataRequests:
     def find_country(self, name: str):
         return self.query(
             f"""\
-SELECT ?country ?countryLabel ?countryDescription WHERE {{
+SELECT (?country as ?uri) WHERE {{
     ?country rdfs:label "{name}"@en;
         wdt:P31 wd:Q3624078. # 'instance of' 'sovereign state'
     FILTER(NOT EXISTS {{ ?country (p:P31/ps:P31) wd:Q3024240. }}) # is NOT a 'historical country'
     SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
 }}
 LIMIT 1""",
-            schema=["country", "countryLabel", "countryDescription"],
-        )
+            schema=["uri"],
+        ).select(pl.lit(name).alias("label"), pl.col("*"))
 
     def find_bordering(self, id: str):
         return self.query(
             f"""\
-SELECT ?bordering ?borderingLabel WHERE {{
+SELECT (?bordering as ?borderingUri) ?borderingLabel WHERE {{
     ?bordering wdt:P31 wd:Q3624078. # 'instance of' 'sovereign state'
     ?bordering wdt:P47 wd:{id}. # 'shares border with'
     FILTER(NOT EXISTS {{ ?bordering (p:P31/ps:P31) wd:Q3024240. }}) # is NOT a 'historical country'
     SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
 }}""",
-            schema=["bordering", "borderingLabel"],
-        )
+            schema=["borderingUri", "borderingLabel"],
+        ).select(pl.lit(id).alias("id"), pl.col("*"))
 
 
 wikidata = WikidataRequests()
+
+
+# %%
+def id_from_uri(uri: str):
+    # http://www.wikidata.org/entity/Q39 -> Q39
+    return uri.rsplit("/", 1)[1]
+
+
+# %%
+input_countries = (
+    pl.scan_csv("./data/landlocked 2023-06-26 - Лист1.csv")
+    .select(pl.col("landlocked country").alias("label"))
+    .collect()
+    .to_series()
+    .to_list()
+)
+input_countries
 
 
 # %%
@@ -66,33 +73,45 @@ def find_countries(countries):
     dfs = []
     for country in tqdm(countries, total=len(countries)):
         dfs.append(wikidata.find_country(country))
-    return pl.DataFrame({"query": countries}).join(
-        pl.concat(dfs), left_on="query", right_on="countryLabel", how="left"
+    return pl.DataFrame({"label": countries}).join(
+        pl.concat(dfs), on="label", how="left"
     )
 
 
 # %%
-found_countries = find_countries(input_countries)
-found_countries
-
-
-# %%
+country_codes = find_countries(input_countries)
 print(
     "Found",
-    len(found_countries.filter(pl.col("country").is_not_null())),
+    len(country_codes.filter(pl.col("label").is_not_null())),
     "/",
-    len(found_countries),
+    len(country_codes),
 )
 
+# %%
+country_codes = country_codes.with_columns(pl.col("uri").apply(id_from_uri).alias("id"))
+country_codes
+
 
 # %%
-def extract_id(uri: str):
-    return uri.rsplit("/", 1)[1]
+def find_bordering_all(ids: list[str]):
+    dfs = []
+    for id in tqdm(ids, total=len(ids)):
+        dfs.append(wikidata.find_bordering(id))
+    return pl.concat(dfs)
 
 
-found_countries.with_columns(pl.col("country").apply(extract_id).alias("id"))
 # %%
-b = wikidata.find_bordering("Q403")
-b
+bordering = find_bordering_all(country_codes["id"].to_list())
+bordering
 # %%
-b.with_columns(pl.lit("Q403").alias("id"))
+final = country_codes.join(bordering, on="id").select(
+    [
+        pl.col("label"),
+        pl.col("id"),
+        pl.col("borderingLabel"),
+        pl.col("borderingUri").apply(id_from_uri).alias("borderingId"),
+    ]
+)
+final
+# %%
+final.write_csv("./data/bordering.csv")
